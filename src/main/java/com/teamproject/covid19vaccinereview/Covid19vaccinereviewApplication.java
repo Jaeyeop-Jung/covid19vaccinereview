020607264e17a61aecee903bedd13367fe6a38a6
Jaeyeop-Jung <jjy5349@naver.com>
2022-01-09 18:04:20 +0900

    initialized

---
+++ src/main/java/com/teamproject/covid19vaccinereview/Covid19vaccinereviewApplication.java
@@ -0,0 +1,13 @@
+package com.teamproject.covid19vaccinereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19vaccinereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19vaccinereviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamproject/covid19vaccinereview/Covid19vaccinereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.covid19vaccinereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19vaccinereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
