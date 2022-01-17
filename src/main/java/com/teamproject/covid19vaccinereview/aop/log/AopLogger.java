a2b2f92e6ab21fc09fab3813e9f41fd2ba760f4f
오형진/플랫폼개발파트/TO <gino@nhnsoft.com>
2022-01-17 22:50:35 +0900

    feat: AOP 사용하여 로깅 기능 확인 (+테스트)

--- src/main/java/com/teamproject/covid19vaccinereview/aop/log/AopLogger.java
+++ src/main/java/com/teamproject/covid19vaccinereview/aop/log/AopLogger.java
@@ -1,11 +1,34 @@
 package com.teamproject.covid19vaccinereview.aop.log;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 @Slf4j
 public class AopLogger {
+
+    // PointCut 은 적용할 패키지/메서드 등 위치
+    @Pointcut("within(com.teamproject.covid19vaccinereview.api.*)")
+    private void elapsedTimeLoggingPointCut() { }
+
+    @Pointcut("execution(* com.teamproject.covid19vaccinereview.api.UserApiController.login())")
+    private void loginUserInterceptorPointCut() { }
+
+    // Around / Before / After / AfterThrowing 등 대상에 대해 동작시킬 전후 위치
+    @Around("elapsedTimeLoggingPointCut()")
+    public void elapsedTimeLogging() {
+        System.out.println("Around elapsedTimeLogging()");
+        log.info("Around elapsedTimeLogging()");
+    }
+
+    @Before("loginUserInterceptorPointCut()")
+    public void loginUserInterceptor() {
+        System.out.println("Before loginUserInterceptor()");
+        log.info("Before loginUserInterceptor()");
+    }
 
     void logging() {
         log.info("info AopLogger class logging()");
