9d70a68145d460fef4166665081737434c98baf3
Jaeyeop-Jung <jjy5349@naver.com>
2022-01-18 17:22:10 +0900

    Swagger 적용 (스프링 버젼 2.4.5로 낮춤)

--- src/main/java/com/teamproject/covid19vaccinereview/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/teamproject/covid19vaccinereview/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,8 @@
 package com.teamproject.covid19vaccinereview.filter;
 
-import com.teamproject.covid19vaccinereview.dto.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -12,7 +10,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
