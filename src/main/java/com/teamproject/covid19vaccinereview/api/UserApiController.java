27f9c5894bce828b2f4bce53086516d7bca2dd5d
오형진/플랫폼개발파트/TO <gino@nhnsoft.com>
2022-01-21 00:38:19 +0900

    refactor: join(가입), login(로그인) 컨트롤러 수정

--- src/main/java/com/teamproject/covid19vaccinereview/api/UserApiController.java
+++ src/main/java/com/teamproject/covid19vaccinereview/api/UserApiController.java
@@ -2,24 +2,33 @@ package com.teamproject.covid19vaccinereview.api;
 
 import com.teamproject.covid19vaccinereview.dto.UserDto;
 import com.teamproject.covid19vaccinereview.service.UserService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
+import java.io.IOException;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequiredArgsConstructor
 @Slf4j
 public class UserApiController {
 
     private final UserService userService;
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public UserApiController(UserService userService,
+        UserDetailsService userDetailsService) {
+        this.userService = userService;
+        this.userDetailsService = userDetailsService;
+    }
 
     @PostMapping("/originlogin")
     public @ResponseBody String originLogin(HttpServletRequest request, HttpServletResponse response, @RequestBody UserDto userDto){
