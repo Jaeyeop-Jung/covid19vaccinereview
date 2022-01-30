0987038569b2d339b3233fd39c52616c448a1e11
Jaeyeop-Jung <jjy5349@naver.com>
2022-01-31 02:17:25 +0900

    무중단 배포 테스트

--- src/main/java/com/teamproject/covid19vaccinereview/controller/NginxController.java
+++ src/main/java/com/teamproject/covid19vaccinereview/controller/NginxController.java
@@ -1,16 +1,34 @@
 package com.teamproject.covid19vaccinereview.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@Log4j2
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
 public class NginxController {
 
+    private final Environment env;
+
     @GetMapping({"/nginxConfig"})
-    public @ResponseBody String nginxConfig(){
-        return "정상 작동";
+    public String nginxConfig(){
+
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
---
+++ src/test/java/com/teamproject/covid19vaccinereview/controller/NginxControllerTest.java
@@ -0,0 +1,59 @@
+package com.teamproject.covid19vaccinereview.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NginxControllerTest {
+
+    @Test
+    public void real_profile_이_조회된다(){
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        NginxController nginxController = new NginxController(env);
+
+        //when
+        String profile = nginxController.nginxConfig();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫번째가_조회된다() {
+        //given
+        String expectedProfile = "ouath";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        NginxController nginxController = new NginxController(env);
+
+        //when
+        String profile = nginxController.nginxConfig();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다(){
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        NginxController nginxController = new NginxController(env);
+
+        //when
+        String profile = nginxController.nginxConfig();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
