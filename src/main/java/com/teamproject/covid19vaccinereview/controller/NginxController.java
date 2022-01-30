702a0436815edea79ec35155e83d826ba6411665
Jaeyeop-Jung <jjy5349@naver.com>
2022-01-31 01:17:53 +0900

    무중단 배포 테스트

---
+++ src/main/java/com/teamproject/covid19vaccinereview/controller/NginxController.java
@@ -0,0 +1,17 @@
+package com.teamproject.covid19vaccinereview.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Log4j2
+public class NginxController {
+
+    @GetMapping({"/nginxConfig"})
+    public @ResponseBody
+    String nginxConfig(){
+        return "정상 작동";
+    }
+}
