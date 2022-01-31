8e7665220d3c72bcda1e3eff4ffbf01911f9ae61
Jaeyeop-Jung <jjy5349@naver.com>
2022-01-31 23:29:42 +0900

    무중단 배포 테스트 health.sh, profile.sh 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-# 쉬고 있는 profile 찾기
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
+# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"http://127.0.0.1/nginxConfig)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/nginxConfig)
 
-    if [ ${RESPONSE_CODE} -ge 400]
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=$(curl -s http://127.0.0.1/nginxConfig)
+        CURRENT_PROFILE=$(curl -s http://localhost/nginxConfig)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
     then
-        IDLE_PROFILE=real2
+      IDLE_PROFILE=real2
     else
-        IDLE_PROFILE=real1
+      IDLE_PROFILE=real1
     fi
 
     echo "${IDLE_PROFILE}"
@@ -28,10 +29,10 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1]
+    if [ ${IDLE_PROFILE} == real1 ]
     then
-        echo "8081"
+      echo "8081"
     else
-        echo "8082"
+      echo "8082"
     fi
 }
