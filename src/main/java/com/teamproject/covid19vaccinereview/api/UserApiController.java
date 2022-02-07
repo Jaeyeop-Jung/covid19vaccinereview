834e1917ab203f834c6318eac2f394af9d029e06
Jaeyeop-Jung <jjy5349@naver.com>
2022-02-07 16:23:45 +0900

    hotfix: @Valid 어노테이션을 통한 파라미터 바인딩 기능 추가

--- src/main/java/com/teamproject/covid19vaccinereview/api/UserApiController.java
+++ src/main/java/com/teamproject/covid19vaccinereview/api/UserApiController.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
