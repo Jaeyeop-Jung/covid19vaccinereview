a02452d3b3cc79d840a6abe37c8f75e6e929929c
Jaeyeop-Jung <jjy5349@naver.com>
2022-03-10 09:03:31 +0900

    feature: Notification SSE 통신 테스트용

--- src/main/java/com/teamproject/covid19vaccinereview/api/NotificationApiController.java
+++ src/main/java/com/teamproject/covid19vaccinereview/api/NotificationApiController.java
@@ -1,6 +1,7 @@
 package com.teamproject.covid19vaccinereview.api;
 
 import com.teamproject.covid19vaccinereview.service.NotificationService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,11 +21,13 @@ public class NotificationApiController {
     private final NotificationService notificationService;
 
     @GetMapping("/sub")
+    @ApiOperation(value = "SSE 테스트", notes = "SSE를 통해서 백엔드 서버를 계속 구독")
     public SseEmitter sub(HttpServletRequest request){
         return notificationService.subscribe(request);
     }
 
     @PostMapping("/memo")
+    @ApiOperation(value = "SSE 메시지 전송 테스트", notes = "백엔드에 SSE로 연결된 모든 유저에게 메시지 전송")
     public void memo(@RequestParam String content) throws IOException {
         notificationService.memo(content);
         System.out.println("메시지 전송 성공");
